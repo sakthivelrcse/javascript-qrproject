76266ce0e9e28468e94808c70e69fc085b0f899d
svishere <sakthivel100gym@gmail.com>
2024-06-28 22:59:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let imgbox = document.getElementById("imgbox")
+let qrimage = document.getElementById("qrimage")
+let qrtext = document.getElementById("qrtext")
+function generateQR(){
+    qrimage.scr = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
+}
